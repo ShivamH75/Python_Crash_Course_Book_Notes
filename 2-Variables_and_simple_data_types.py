ed2c7a83a40d9912683351e135864b6c96e1da82
ShivamH75 <hundekari.shivam45@gmail.com>
2023-06-10 23:51:32 +0530

    initial commit

--- 2-Variables_and_simple_data_types.py
+++ 2-Variables_and_simple_data_types.py
@@ -36,3 +36,29 @@ print(stringWithPrefixAndSuffix.removeprefix('https://').removesuffix('.com'))
 # Escape with single or double quotes as a part of string 
 print("He said \"Hello\"")
 print('He shouted, \'Run!\'')   
+
+
+# ________________________________________________________________________________
+# Numbers 
+# 1. To represent power - use double **
+print(2**3)         # cube of 2
+
+# 2. calculations take place as per BODMAS rule
+print(2 + 5*8 -2)
+
+# 3. int + float combinations
+print(4.0/2.0)          # float / float = float
+print(4/2)          # int / int = float
+
+
+# Misc info 
+# 1. Assign multiple variables in a single line
+x,y,z = 'Hello', 23, 3.14
+print (x,y,z)
+
+# 2. Provide underscre in numbers for better readibility - will get ignored in code
+oneCrore = 1_00_00_000
+print(oneCrore)
+
+# 3. CONSTANTS - Py dont have inbuilt constant types, but CAPS can be used as good practice
+PI_VALUE = 3.14

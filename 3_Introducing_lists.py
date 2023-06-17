557606e3e2899a86965a95f3d21c502db22aa177
ShivamH75 <hundekari.shivam45@gmail.com>
2023-06-17 11:38:07 +0530

    3rd file

---
+++ 3_Introducing_lists.py
@@ -0,0 +1,8 @@
+# list example 
+
+fruits = ["apple", "banana", "mango"]
+
+print(fruits)   #if list is print directly, it prints with quotations and brackets
+
+print(fruits[0])    # prints 0th element of list
+

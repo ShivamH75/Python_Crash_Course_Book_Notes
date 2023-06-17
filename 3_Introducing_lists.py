8cc0f1a63936ba0b0d4b50d681791ec49e52f977
ShivamH75 <hundekari.shivam45@gmail.com>
2023-06-17 12:52:31 +0530

    updating lists

--- 3_Introducing_lists.py
+++ 3_Introducing_lists.py
@@ -25,7 +25,45 @@ print(fruits)
 
 # 4. Delete element from list by its Index
 
-del fruits[0]
+del fruits[0]           #this can also delete whole variable
 
 print(fruits)
+
+# 5. Remove item by its position - POP()
+
+bikes = ["yamaha", "honda", "bajaj", "ducati"]
+
+removedBike = bikes.pop(1)              
+#removes element at 1th place. if nothing is mentioned in (), then 0th element will get popped up
+
+print(bikes)
+print(removedBike)          #removed element can be stored 
+
+# 6. Remove item by its value
+bikes.remove("bajaj")
+print(bikes)
+
+## ORGANIZING A LIST
+cars1 = ["bmw", "ford", "audi", "honda", "toyota", "lambo"]
+
+# 1. Sort list PERMANENTLY 
+cars1.sort(reverse=False)        #to sort in reverse, put "False"
+
+print(cars1)
+# use another variable for not losing original order of list elements
+
+# 2. Sort list TEMPORARILY
+cars2 = ["bmw", "ford", "audi", "honda", "toyota", "lambo"]
+
+print("Here is temporarily sorted list- ", sorted(cars2))
+print ("Here is original list again -", cars2)
+
+# NOTE: Sort will not work properly if some elements are with lowercase and some with uppercase
+
+# 3. Print list in reverse
+cars2.reverse()
+print(cars2)
+
+# 4. Find number of elements of list
+print(len(cars2))
 

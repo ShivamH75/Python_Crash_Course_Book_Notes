ad78ae6259b9582a58a676ad449f3b3a962c8c1d
ShivamH75 <hundekari.shivam45@gmail.com>
2023-06-17 15:03:48 +0530

    4th chapter committed

--- 4-working_with_lists.py
+++ 4-working_with_lists.py
@@ -1,1 +1,8 @@
-helo
+# # Looping through lists:
+# Print every element of the list
+
+cars = ["bmw", "ford", "audi", "honda", "toyota", "lamborghini"]
+
+for car in cars:
+    print(car)
+# for every car in 'cars' print the name of the car

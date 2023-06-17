fa229dba29d880241d38baef429a2519b2e642d7
ShivamH75 <hundekari.shivam45@gmail.com>
2023-06-17 12:18:18 +0530

    updating lists

--- 3_Introducing_lists.py
+++ 3_Introducing_lists.py
@@ -5,3 +5,27 @@ fruits = ["apple", "banana", "mango"]
 print(fruits)   #if list is print directly, it prints with quotations and brackets
 
 print(fruits[0])    # prints 0th element of list
+
+# Add elements to the list
+# 1. add at last - AppEND 
+
+fruits.append("cherry") #append is inbuilt function to add elements at last. function is added at last of variable name.
+print(fruits)
+
+# 2. modify elements of the list
+
+fruits[0] = "Jackfruit"     #replaces the 0th element
+print(fruits)
+
+# 3. Insert element in the list without deleting already present elements 
+# syntax: list_name.insert(index, "element name")
+fruits.insert(2, "Strawberry")
+
+print(fruits)
+
+# 4. Delete element from list by its Index
+
+del fruits[0]
+
+print(fruits)
+

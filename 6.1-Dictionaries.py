0e3b111aaf9285064af945f5af8d052905703a47
ShivamH75 <hundekari.shivam45@gmail.com>
2023-07-22 18:50:30 +0530

    Dict comit

--- 6.1-Dictionaries.py
+++ 6.1-Dictionaries.py
@@ -1,3 +1,23 @@
 # Dictionaries help to  connect related information
 # Formt of dictioary is Key-Value pairs in Curly Brackets
 
+sampleDict = {
+    'name': "Shivam",
+    'hobby': "Coding",
+    "Age" : 23
+}
+
+for item in sampleDict:
+    print (sampleDict[item])
+
+#  To Add new key-values in existing dictionary
+#  format :: dictName['newKey'] = 'newValue'
+
+sampleDict['company'] = 'Mars ASSOCIATION'
+sampleDict['Car'] = 'Audi R8'
+
+print (sampleDict)
+
+#  To modify value of any kwy within a dictionary
+sampleDict['Car'] = 'Mercedes Benz G-Wagon'
+print (sampleDict)

b63d3cb2978ef9397900346c62dd59cde63fab82
ShivamH75 <hundekari.shivam45@gmail.com>
2023-08-05 22:39:36 +0530

    okay to add

--- 8.3-Functions_pt4.py
+++ 8.3-Functions_pt4.py
@@ -20,3 +20,49 @@ def pizzaToppingList(*toppings):
 
 pizzaToppingList('onion')
 pizzaToppingList('olive', 'cheese', 'capsicum', 'mushroom')
+
+
+
+# # Mixing Positional and Arbitrary Arguments
+# it is suggested to use positional, relational and then any other type of arguments
+
+# eg. function to recieve fixed info in one parameter, and arbitrary info in another parameters
+def mixedPizza(size, category, *toppings):
+    print(f"Your {size} sized {category} pizza has following toppings: ")
+    for topping in toppings:
+        print(topping.title())
+
+mixedPizza('medium', 'veg', 'capsicum', 'olive', 'chillie')
+# # here, the first 2 arguments acts as positional arguments, later all followed by Arbitrary
+# however, if we use relational arguments with Arbitrary arguments, we get error - SyntaxError: positional argument follows keyword argument
+
+
+## Using Arbitrary Keyword Arguments
+# consider for storing key-value type data in dict, there is a case that some pairs are known, but many k-v pairs can be Arbitrary. 
+# so we use "Arbitrary Keyword Arguments", which has two * before the parameter name
+
+# def userDict(name, surname, **allOther):
+#     newDict = {}
+#     name = input("Enter your name: ")
+#     surname = input("Enter your surname: ")
+#     anyOtherInfo = input("Do you wanted to add any other info (y/n)? ")
+#     if anyOtherInfo == 'y':
+#         newInputField = input("Add field name: ")
+#         newInputValue = input("Add field value: ")
+#     else:
+#         print("thank you")
+
+#     newDict['name'] = name
+#     newDict['surname'] = surname     .................... practice problem
+
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info                         #returns all other values as dict
+
+
+user_profile = build_profile('albert', 'einstein', location='princeton',field='physics')
+print(user_profile)
+# positional args will be displayed at the end in the o/p
+
